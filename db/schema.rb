41dd7c3c4978c521eb5b83674964aa83d8010b4b
lmdsultan <mdgetbetter.gmail.com>
2020-02-07 16:46:38 +0300

    migrate the file of incident migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_124836) do
+ActiveRecord::Schema.define(version: 2020_02_07_132435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 2020_02_07_124836) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "incidents", force: :cascade do |t|
+    t.string "issue"
+    t.bigint "system_id", null: false
+    t.text "solution"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["system_id"], name: "index_incidents_on_system_id"
+  end
+
   create_table "systems", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.bigint "admin_id", null: false
@@ -36,5 +45,6 @@ ActiveRecord::Schema.define(version: 2020_02_07_124836) do
     t.index ["admin_id"], name: "index_systems_on_admin_id"
   end
 
+  add_foreign_key "incidents", "systems"
   add_foreign_key "systems", "admins"
 end
