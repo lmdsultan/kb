b8c8371fdc339d0b656aae1f7f25b5cc56f6039a
lmdsultan <mdgetbetter.gmail.com>
2020-02-07 16:41:28 +0300

    declare associations between Admin and Incident which admin has many incidents through systems

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,6 @@
 class Admin < ApplicationRecord
   has_many :systems
+  has_many :incidents, through: :systems
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
