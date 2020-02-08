fea1d076a83d15ee6246fb0f3f9ced2792ca963c
lmdsultan <mdgetbetter.gmail.com>
2020-02-08 18:40:42 +0300

    add solve params to permit

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -71,6 +71,6 @@ class IncidentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def incident_params
-      params.require(:incident).permit(:issue, :solution)
+      params.require(:incident).permit(:issue, :solution, :solve)
     end
 end
