58035243d3f14f6f0ef3a4780a03652a3ba0da99
lmdsultan <mdgetbetter.gmail.com>
2020-02-08 19:17:42 +0300

    overwrite the path of controller because I overwrite generated users controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
     resources :incidents
   end
   devise_for :admins, controllers: { registrations: 'admins/registrations',
-                                    sessions: 'admins/sessions', unlocks: 'admins/unlocks',
-                                    passwords: 'admins/passwords', confirmations: 'admins/confirmations'}
+                                      sessions: 'admins/sessions', unlocks: 'admins/unlocks',
+                                      passwords: 'admins/passwords', confirmations: 'admins/confirmations'}
+
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    sessions: 'users/sessions', unlocks: 'users/unlocks',
+                                    passwords: 'users/passwords', confirmations: 'users/confirmations'}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
